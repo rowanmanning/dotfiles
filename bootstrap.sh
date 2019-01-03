1a2a1e899115e165859d31be1faf4c91e33c6d26
Rowan Manning <accounts+github@rowanmanning.com>
2019-01-03 15:41:59 +0000

    Remove tmuxinator config

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ git pull origin master
 function doIt() {
 
     echo "Creating symlinks..."
-    rm -rf ~/.bash_profile ~/.bashrc ~/.gitconfig ~/.inputrc ~/Library/Application\ Support/Code/User/settings.json ~/.vim ~/.vimrc ~/.tmux.conf ~/.tmuxinator
+    rm -rf ~/.bash_profile ~/.bashrc ~/.gitconfig ~/.inputrc ~/Library/Application\ Support/Code/User/settings.json ~/.vim ~/.vimrc ~/.tmux.conf
     ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
     ln -s ~/.dotfiles/.bashrc ~/.bashrc
     ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
@@ -13,7 +13,6 @@ function doIt() {
     ln -s ~/.dotfiles/.vim ~/.vim
     ln -s ~/.dotfiles/.vimrc ~/.vimrc
     ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-    ln -s ~/.dotfiles/.tmuxinator ~/.tmuxinator
     ln -s ~/.dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
     touch ~/.hushlogin
 
