f2b30cd0be3e62130f0d6002bbb148b2c92d87ad
Rowan Manning <info@rowanmanning.com>
2020-02-26 14:20:28 +0000

    Move to zsh

--- .bashrc
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-[ -n "$PS1" ] && source $HOME/.bash_profile
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,19 +4,25 @@ echo "Updating dotfiles..."
 git pull origin master
 function doIt() {
 
+    if [ -d ~/.oh-my-zsh ]; then
+        echo "Oh My Zsh already installed"
+    else
+        echo "Installing Oh My Zsh..."
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    fi
+
     echo "Creating symlinks..."
-    rm -rf ~/.bash_profile ~/.bashrc ~/.gitconfig ~/.inputrc ~/Library/Application\ Support/Code/User/settings.json ~/.vim ~/.vimrc
-    ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
-    ln -s ~/.dotfiles/.bashrc ~/.bashrc
+    rm -rf ~/.gitconfig ~/.iterm ~/Library/Application\ Support/Code/User/settings.json ~/.vim ~/.vimrc ~/.zshrc
+    ln -s ~/.dotfiles/.zshrc ~/.zshrc
     ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-    ln -s ~/.dotfiles/.inputrc ~/.inputrc
+    ln -s ~/.dotfiles/.iterm ~/.iterm
     ln -s ~/.dotfiles/.vim ~/.vim
     ln -s ~/.dotfiles/.vimrc ~/.vimrc
     ln -s ~/.dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
     touch ~/.hushlogin
 
-    echo "Loading bash_profile..."
-    source ~/.bash_profile
+    echo "Loading profile..."
+    zsh
     echo "Done!"
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
